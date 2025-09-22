6459f15f045ccf450adb5f13c8f1832b881f325d
石金龙 <45546443+ShiJinlong1997@users.noreply.github.com>
2025-09-22 02:15:46 +0000

    feat: signin box

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function main() {
+function initParallaxElems() {
   const parallaxElems = Array.from(document.querySelectorAll('.parallax'));
   addEventListener("pointermove", function(event) {
     const percentage = {
       x: event.clientX / innerWidth - 0.5,
       y: event.clientY / innerHeight - 0.5,
     };
-
+  
     parallaxElems.forEach(function(elem) {
       const translate = {
         x: percentage.x * Number(elem.dataset.speed) * 20,
@@ -14,6 +14,27 @@ function main() {
       elem.style.translate = `${translate.x}px ${translate.y}px`;
     });
   });
+}
+
+function initSigninPageBox() {
+  const showButton = document.querySelector('#show-signin-button');
+  const box = document.querySelector('.signin-page-box');
+  const closeButton = document.querySelector('.signin-page-box .close');
+
+  showButton.addEventListener('click', function() {
+    box.classList.remove('close-signin');
+    box.classList.add('open-signin');
+  });
+
+  closeButton.addEventListener('click', function() {
+    box.classList.remove('open-signin');
+    box.classList.add('close-signin');
+  });
+}
+
+function main() {
+  initParallaxElems();
+  initSigninPageBox();
 }
 
 main();
